5e096aad135bd350a20836e9aca4b3ddf3d1ede1
Chris Moss <hcmoss70@gmail.com>
2023-03-04 00:54:23 -0500

    added icons to action bar

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -1,4 +1,21 @@
+import { FaBold, FaItalic, FaStrikethrough, FaUnderline } from "react-icons/fa"
 const ActionBar = () => {
-  return <div className="actionContainer"></div>
+  return (
+    <div className="actionContainer">
+      <div className="buttonContainer">
+        <button>
+          <FaBold />
+        </button>
+        <div className="divider" />
+        <button>
+          <FaItalic />
+        </button>
+        <div className="divider" />
+        <button>
+          <FaUnderline />
+        </button>
+      </div>
+    </div>
+  )
 }
 export default ActionBar
