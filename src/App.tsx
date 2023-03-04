dd4a174897c53c6ab1fe44908b267043c6c000e7
Chris Moss <hcmoss70@gmail.com>
2023-03-04 01:25:09 -0500

    set up seperate html card

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,27 @@ import ActionBar from "./components/ActionBar"
 function App() {
   return (
     <div className="App">
-      <div className="card">
-        <div className="centerText">
-          <h1>Rich Text Editor</h1>
+      <div className="centerText">
+        <h1>Text to HTML Converter</h1>
+      </div>
+      <div className="flexBox">
+        <div className="card">
+          <div className="centerText">
+            <h1>Text Editor</h1>
+          </div>
+          <ActionBar />
+          <div>
+            <div className="textContainer" contentEditable="true"></div>
+          </div>
         </div>
-        <ActionBar />
-        <div className="textContainer" contentEditable="true"></div>
+        <div className="divider" />
+        <div className="divider" />
+        <div className="divider" />
+        <div className="card">
+          <div className="centerText">
+            <h1>HTML Code</h1>
+          </div>
+        </div>
       </div>
     </div>
   )
