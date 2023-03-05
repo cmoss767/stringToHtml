c17a66654215ca7ede463ffac4f9097d3e9353f7
Chris Moss <hcmoss70@gmail.com>
2023-03-04 23:02:00 -0500

    can change editable text styles

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -1,18 +1,64 @@
 import { FaBold, FaItalic, FaStrikethrough, FaUnderline } from "react-icons/fa"
 const ActionBar = () => {
+  $(document).ready(function () {
+    $("#Bold").click(function () {
+      document.execCommand("bold")
+    })
+    $("#Italic").click(function () {
+      document.execCommand("italic")
+    })
+  })
   return (
     <div className="actionContainer">
       <div className="buttonContainer">
-        <button>
+        <button
+          key="bold"
+          onMouseDown={(evt) => {
+            evt.preventDefault()
+            document.execCommand("bold", false)
+          }}
+        >
           <FaBold />
         </button>
         <div className="divider" />
-        <button>
+        <button
+          key="italic"
+          onMouseDown={(evt) => {
+            evt.preventDefault()
+            document.execCommand("italic", false)
+          }}
+        >
           <FaItalic />
         </button>
         <div className="divider" />
-        <button>
+        <button
+          key="underline"
+          onMouseDown={(evt) => {
+            evt.preventDefault() // Avoids loosing focus from the editable area
+            document.execCommand("underline", false) // Send the command to the browser
+          }}
+        >
           <FaUnderline />
+        </button>
+        <div className="divider" />
+        <button
+          key="formatBlock"
+          onMouseDown={(evt) => {
+            evt.preventDefault() // Avoids loosing focus from the editable area
+            document.execCommand("formatBlock", false, "h1") // Send the command to the browser
+          }}
+        >
+          H1
+        </button>
+        <div className="divider" />
+        <button
+          key="formatBlock"
+          onMouseDown={(evt) => {
+            evt.preventDefault() // Avoids loosing focus from the editable area
+            document.execCommand("formatBlock", false, "h3") // Send the command to the browser
+          }}
+        >
+          H3
         </button>
       </div>
     </div>
